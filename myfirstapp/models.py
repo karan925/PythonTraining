4cedb314526f0b1562b1cecdefc049e855159e74
karan <ksingh209@gmail.com>
2020-01-05 23:43:40 -0800

    Created Superuser and Added Portfolio to models

--- myfirstapp/models.py
+++ myfirstapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Portfolio(models.Model):
+    name = models.CharField(max_length=10, unique=False)
+    gender = models.CharField(max_length=5, unique=False)
+    age = models.DateField()
+    address = models.CharField(max_length=50, unique=False)
